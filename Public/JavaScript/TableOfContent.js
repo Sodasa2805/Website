514ea52d584150362c18dde32147c21284068795
Sodasa2805 <115702576+Sodasa2805@users.noreply.github.com>
2023-10-22 18:39:48 +0200

    Update TableOfContent.js

--- Public/JavaScript/TableOfContent.js
+++ Public/JavaScript/TableOfContent.js
@@ -1,13 +1,42 @@
+var pageToGoTo = "";
+
 function tableOfContent(goTo) {
 
     if (goTo != '') {
         window.location = goTo;
+
+        pageToGoTo = goTo;
+
+        sessionStorage.setItem("pageGoneTo", pageToGoTo);
     }
 }
 
 var selectPage = document.getElementById('select');
 
-selectPage.onchange = function() {
-    var value = this.value;
-    tableOfContent(value); 
-};
+selectPage.addEventListener("change", function () {
+    var value = selectPage.value;
+    tableOfContent(value);
+});
+
+var pageGoneTo = sessionStorage.getItem("pageGoneTo");
+
+function reorderOptions() {
+
+    var optGroup1 = document.getElementById('moveAround');
+
+    var selectedValue = pageGoneTo;
+
+    var options = selectPage.options;
+
+    for (let i = options.length - 1; i >= 0; i--) {
+        if (options[i].value === selectedValue) {
+            optGroup1.insertBefore(options[i], options[0]);
+            break;
+        }
+    }
+
+    selectPage.selectedIndex = 0;
+}
+
+reorderOptions();
+
